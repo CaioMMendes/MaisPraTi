74f0ac9046ad0aa7c3a11f32b11ca83847507e16
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-12 16:41:41 -0300

    feat: Add initial findHotelPerCity function

---
+++ modulo01/atividade02/hotel/functions/findHotelPerCity.js
@@ -0,0 +1,56 @@
+const prompt = require("prompt-sync")()
+const fs = require("node:fs/promises")
+
+async function findHotelPerCity() {
+  const city = prompt("Digite o nome da cidade: ").trim().toLowerCase()
+
+  if (city === "") {
+    console.log()
+    console.log("Buscando por todas as cidades")
+    console.log()
+  }
+
+  try {
+    const hotels = await fs.readFile(`${__dirname}/../db.json`)
+    const hotelsData = JSON.parse(hotels)
+
+    const findedHotels = hotelsData.hotels.filter((hotel) =>
+      hotel.city.toLowerCase().includes(city)
+    )
+    console.log()
+    console.log(`Numero de hotéis encontrados`, findedHotels.length)
+    console.log("Hotéis encontrados:")
+    if (findedHotels.length < 1) {
+      return console.log(
+        `Não foi possível encontrar nenhum hotel buscando por "${city}".`
+      )
+    }
+
+    for (let {
+      reviews,
+      name,
+      city,
+      totalRooms,
+      avaliableRooms,
+    } of findedHotels) {
+      const review =
+        reviews.numberOfReviews !== 0 ? reviews.reviewsMedia : "Sem avaliações"
+
+      console.table({
+        "Nome": name,
+        "Cidade": city,
+        "Número de quartos": totalRooms,
+        "Quartos disponíveis": avaliableRooms,
+        "Avaliações": review,
+      })
+      console.log()
+    }
+  } catch (error) {
+    console.log()
+    console.log(error)
+    console.log()
+    console.log("Ocorreu um erro ao tentar buscar os hotéis.")
+  }
+}
+
+module.exports = findHotelPerCity
