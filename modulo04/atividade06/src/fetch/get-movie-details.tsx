213943a0d88593548a401ac2d19acbf6ee57dfb3
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-08 02:43:28 -0300

    Add get movie details function

---
+++ modulo04/atividade06/src/fetch/get-movie-details.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+interface GetMovieDetailsProps {
+  id?: number;
+}
+
+export default async function getMovieDetails({ id }: GetMovieDetailsProps) {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const token = import.meta.env.VITE_API_TOKEN;
+  const response = await axios(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&include_adult=false&language=pt-BR`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer Authorization: Bearer ${token}`,
+      },
+    },
+  );
+  if (response.status >= 200 && response.status < 300) {
+    const movies = await response.data;
+    console.log(movies);
+    return {
+      status: response.status,
+      message: "Filmes encontrados com sucesso",
+      movies: movies,
+    };
+  } else {
+    return {
+      status: response.status,
+      message: "Ocorreu um erro ao tentar realizar a busca",
+    };
+  }
+}
