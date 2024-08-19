9aefec77578c08ddd094f9c4fec9d8fe4b3ecae7
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-19 16:25:21 -0300

    feat: Add use-window-dimensions hook

---
+++ modulo04/atividade06/src/pages/home/components/use-window-dimensions.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export { useWindowDimensions };
