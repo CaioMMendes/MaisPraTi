60bc1b76d191f42cf87a5a41256ca943d007f1b8
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-22 14:43:52 -0300

    feat: Add form and toast messages to IPAddressFinder

--- modulo04/atividade05/src/pages/IPAddressFinder.jsx
+++ modulo04/atividade05/src/pages/IPAddressFinder.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'; // Importa o hook useState do React
-import axios from 'axios'; // Importa a biblioteca axios para fazer requisições HTTP
-import styled from 'styled-components'; // Importa styled-components para estilizar os componentes
+import { useState } from "react" // Importa o hook useState do React
+import axios from "axios" // Importa a biblioteca axios para fazer requisições HTTP
+import styled from "styled-components" // Importa styled-components para estilizar os componentes
+import { toastError, toastSuccess } from "../utils/toast"
 
 // Define o estilo do container principal
 const Container = styled.div`
@@ -14,7 +15,7 @@ const Container = styled.div`
   box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
   max-width: 400px;
   margin: 50px auto;
-`;
+`
 
 // Define o estilo do título
 const Title = styled.h2`
@@ -22,7 +23,7 @@ const Title = styled.h2`
   margin-bottom: 20px;
   font-size: 24px;
   text-align: center;
-`;
+`
 
 // Define o estilo do campo de entrada
 const Input = styled.input`
@@ -39,7 +40,7 @@ const Input = styled.input`
     border-color: #007bff;
     outline: none;
   }
-`;
+`
 
 // Define o estilo do botão
 const Button = styled.button`
@@ -55,7 +56,7 @@ const Button = styled.button`
   &:hover {
     background-color: #0056b3;
   }
-`;
+`
 
 // Define o estilo do container de resultados
 const ResultsContainer = styled.div`
@@ -65,43 +66,65 @@ const ResultsContainer = styled.div`
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   width: 100%;
-`;
+`
+
+// Cria um formulário estilizado
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`
 
 // Componente principal IPAddressFinder
 const IPAddressFinder = () => {
-  const [ip, setIp] = useState(''); // Define o estado para o IP digitado pelo usuário
-  const [ipData, setIpData] = useState(null); // Define o estado para armazenar os dados do IP
+  const [ip, setIp] = useState("") // Define o estado para o IP digitado pelo usuário
+  const [ipData, setIpData] = useState(null) // Define o estado para armazenar os dados do IP
 
   // Função para buscar os dados do IP
-  const findIP = async () => {
+  const handleFindIp = async (e) => {
+    e.preventDefault()
     try {
       const url = `https://ipinfo.io/${ip}/json`
-      const response = await axios.get(url); // Faz uma requisição GET para a API ipinfo.io
-      setIpData(response.data); // Armazena os dados da resposta no estado ipData
+      const response = await axios.get(url) // Faz uma requisição GET para a API ipinfo.io
+      setIpData(response.data) // Armazena os dados da resposta no estado ipData
+
+      toastSuccess({ text: "Ip encontrado com sucesso!" })
     } catch (error) {
-      console.error("Error fetching IP address data:", error); // Exibe um erro no console em caso de falha
+      console.error("Error fetching IP address data:", error) // Exibe um erro no console em caso de falha
+      toastError({ text: `Não foi possível encontrar o ip: ${ip}` })
     }
-  };
+  }
 
   return (
     <Container>
-      <Title>IP Address Finder</Title>
-      <Input
-        type="text"
-        value={ip} // Valor do campo de entrada é ligado ao estado ip
-        onChange={(e) => setIp(e.target.value)} // Atualiza o estado ip conforme o usuário digita
-        placeholder="Enter IP address" // Placeholder do campo de entrada
-      />
-      <Button onClick={findIP}>Find IP</Button> {/* Botão que chama a função findIP quando clicado */}
+      <Form onSubmit={handleFindIp}>
+        <Title>IP Address Finder</Title>
+        <Input
+          type="text"
+          value={ip} // Valor do campo de entrada é ligado ao estado ip
+          onChange={(e) => setIp(e.target.value)} // Atualiza o estado ip conforme o usuário digita
+          placeholder="Enter IP address" // Placeholder do campo de entrada
+        />
+        <Button>Find IP</Button>
+        {/* Botão que chama a função findIP quando clicado */}
+      </Form>
       {ipData && ( // Condicional que exibe os dados do IP se ipData não for null
         <ResultsContainer>
-          <p><strong>IP:</strong> {ipData.ip}</p>
-          <p><strong>Location:</strong> {ipData.city}, {ipData.region}, {ipData.country}</p>
-          <p><strong>ISP:</strong> {ipData.org}</p>
+          <p>
+            <strong>IP:</strong> {ipData.ip}
+          </p>
+          <p>
+            <strong>Location:</strong> {ipData.city}, {ipData.region},{" "}
+            {ipData.country}
+          </p>
+          <p>
+            <strong>ISP:</strong> {ipData.org}
+          </p>
         </ResultsContainer>
       )}
     </Container>
-  );
-};
+  )
+}
 
-export default IPAddressFinder; // Exporta o componente IPAddressFinder como padrão
+export default IPAddressFinder // Exporta o componente IPAddressFinder como padrão
