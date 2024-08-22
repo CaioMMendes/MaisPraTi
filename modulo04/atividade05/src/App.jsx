3643a8f739ab102dcb3aa7bbc9a2b40ccfc128e7
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-22 01:56:32 -0300

    refactor: move carousel to homePage

--- modulo04/atividade05/src/App.jsx
+++ modulo04/atividade05/src/App.jsx
@@ -1,19 +1,12 @@
 // Importa hooks e componentes do React e bibliotecas externas.
 import { useEffect, useState } from "react"
-import { FaArrowLeft } from "react-icons/fa"
-import { Carousel } from "react-responsive-carousel"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Outlet, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import "./App.css"
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
-import IPAddressFinder from "./pages/IPAddressFinder"
-import LanguageTranslator from "./pages/LanguageTranslator"
 import Login from "./pages/Login"
-import MovieSearchEngine from "./pages/MovieSearchEngine"
-import QRCodeGenerator from "./pages/QRCodeGenarator"
-import QuizApp from "./pages/QuizApp"
-import TodoApp from "./pages/TodoApp"
 
 // Estiliza o contêiner principal do aplicativo.
 const AppContainer = styled.div`
@@ -36,111 +29,31 @@ const MainContent = styled.div`
   overflow: hidden;
 `
 
-// Estiliza o contêiner do carrossel.
-const CarouselContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 80%;
-  height: 70%;
-  margin: auto;
-  background-color: #2c3e50;
-  border-radius: 20px;
-  padding: 20px;
-`
+// // Estiliza o botão de retorno.
+// const ReturnButton = styled.button`
+//   padding: 10px 20px;
+//   background-color: #007bff;
+//   color: white;
+//   border: none;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   margin-top: 20px;
+//   display: flex;
+//   align-items: center;
+//   gap: 5px;
 
-// Estiliza o rodapé do aplicativo.
-const Footer = styled.div`
-  width: 100%;
-  background-color: #2c3e50;
-  color: white;
-  text-align: center;
-  padding: 10px 0;
-  position: absolute;
-  bottom: 0;
-
-  @media (max-width: 768px) {
-    padding: 5px 0;
-    font-size: 12px;
-  }
-`
-
-// Estiliza os itens individuais do carrossel.
-const CarouselItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: linear-gradient(135deg, #ff7e5f, #feb47b);
-  padding: 40px;
-  border-radius: 15px;
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
-  text-align: center;
-  transition: transform 0.3s, box-shadow 0.3s;
-  height: 100%;
-  width: 100%;
-
-  &:hover {
-    transform: scale(1.05);
-    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
-  }
-
-  h2 {
-    margin-bottom: 20px;
-    font-size: 24px;
-    color: white;
-  }
-
-  button {
-    padding: 10px 20px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 10px;
-    font-size: 16px;
-
-    &:hover {
-      background-color: #0056b3;
-    }
-  }
-`
-
-// Estiliza o botão de retorno.
-const ReturnButton = styled.button`
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-top: 20px;
-  display: flex;
-  align-items: center;
-  gap: 5px;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`
-
-// Estiliza o carrossel personalizado.
-const CustomCarousel = styled(Carousel)`
-  width: 100%;
-  .carousel-status {
-    display: none;
-  }
-`
+//   &:hover {
+//     background-color: #0056b3;
+//   }
+// `
 
 // Define o componente principal do aplicativo.
 const App = () => {
   // Cria estados para autenticação, visibilidade da barra de navegação, componente atual, e índice do carrossel.
   const [isAuthenticated, setIsAuthenticated] = useState(true)
 
-  const [currentComponent, setCurrentComponent] = useState(null)
-  const [carouselIndex, setCarouselIndex] = useState(0)
+  // const [currentComponent, setCurrentComponent] = useState(null)
+
   const navigate = useNavigate() // Hook para navegação.
 
   // Efeito colateral que redireciona para a página de login se não estiver autenticado.
@@ -156,36 +69,36 @@ const App = () => {
     navigate("/qrcode-generator")
   }
 
-  // Função para definir o componente atual a ser exibido e atualizar o índice do carrossel.
-  const handleAccess = (index, component) => {
-    setCarouselIndex(index)
-    setCurrentComponent(component)
-  }
+  // // Função para definir o componente atual a ser exibido e atualizar o índice do carrossel.
+  // const handleAccess = (index, component) => {
+  //   setCarouselIndex(index)
+  //   setCurrentComponent(component)
+  // }
 
-  // Função para retornar ao carrossel principal.
-  const handleReturn = () => {
-    setCurrentComponent(null)
-  }
+  // // Função para retornar ao carrossel principal.
+  // const handleReturn = () => {
+  //   setCurrentComponent(null)
+  // }
 
   // Função para renderizar o componente atual com base no estado.
-  const renderComponent = () => {
-    switch (currentComponent) {
-      case "QRCodeGenerator":
-        return <QRCodeGenerator />
-      case "IPAddressFinder":
-        return <IPAddressFinder />
-      case "MovieSearchEngine":
-        return <MovieSearchEngine />
-      case "TodoApp":
-        return <TodoApp />
-      case "QuizApp":
-        return <QuizApp />
-      case "LanguageTranslator":
-        return <LanguageTranslator />
-      default:
-        return null
-    }
-  }
+  // const renderComponent = () => {
+  //   switch (currentComponent) {
+  //     case "QRCodeGenerator":
+  //       return <QRCodeGenerator />
+  //     case "IPAddressFinder":
+  //       return <IPAddressFinder />
+  //     case "MovieSearchEngine":
+  //       return <MovieSearchEngine />
+  //     case "TodoApp":
+  //       return <TodoApp />
+  //     case "QuizApp":
+  //       return <QuizApp />
+  //     case "LanguageTranslator":
+  //       return <LanguageTranslator />
+  //     default:
+  //       return null
+  //   }
+  // }
 
   // Renderiza o componente principal.
   return (
@@ -196,8 +109,9 @@ const App = () => {
         </MainContent>
       ) : (
         <>
-          <Navbar />
-          <MainContent>
+          {/* <Navbar />
+          <MainContent> */}
+          {/* 
             {currentComponent ? (
               <>
                 {renderComponent()}
@@ -206,63 +120,14 @@ const App = () => {
                 </ReturnButton>
               </>
             ) : (
-              <CarouselContainer>
-                <CustomCarousel
-                  showArrows={true}
-                  infiniteLoop={true}
-                  autoPlay={true}
-                  interval={5000}
-                  selectedItem={carouselIndex}
-                  onChange={(index) => setCarouselIndex(index)}
-                >
-                  <CarouselItem>
-                    <h2>QR Code Generator</h2>
-                    <button onClick={() => handleAccess(0, "QRCodeGenerator")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>IP Address Finder</h2>
-                    <button onClick={() => handleAccess(1, "IPAddressFinder")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Movie Search Engine</h2>
-                    <button
-                      onClick={() => handleAccess(2, "MovieSearchEngine")}
-                    >
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Todo App</h2>
-                    <button onClick={() => handleAccess(3, "TodoApp")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Quiz App</h2>
-                    <button onClick={() => handleAccess(4, "QuizApp")}>
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <h2>Language Translator</h2>
-                    <button
-                      onClick={() => handleAccess(5, "LanguageTranslator")}
-                    >
-                      Acessar
-                    </button>
-                  </CarouselItem>
-                </CustomCarousel>
-              </CarouselContainer>
+             
             )}
-            <Footer>© 2024 Your Company | All rights reserved</Footer>
-          </MainContent>
+            */}
+          <Outlet />
+          {/* <Footer />
+          </MainContent> */}
         </>
       )}
-      <Outlet />
     </AppContainer>
   )
 }
