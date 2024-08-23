dc3149381c794f3e2cf8efe352c12d8c18be5df0
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-22 23:54:14 -0300

    refactor: Remove unecessary code in App

--- modulo04/atividade05/src/App.jsx
+++ modulo04/atividade05/src/App.jsx
@@ -4,8 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Outlet, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import "./App.css"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
 import Login from "./pages/Login"
 
 // Estiliza o contêiner principal do aplicativo.
@@ -29,24 +27,6 @@ const MainContent = styled.div`
   overflow: hidden;
 `
 
-// // Estiliza o botão de retorno.
-// const ReturnButton = styled.button`
-//   padding: 10px 20px;
-//   background-color: #007bff;
-//   color: white;
-//   border: none;
-//   border-radius: 5px;
-//   cursor: pointer;
-//   margin-top: 20px;
-//   display: flex;
-//   align-items: center;
-//   gap: 5px;
-
-//   &:hover {
-//     background-color: #0056b3;
-//   }
-// `
-
 // Define o componente principal do aplicativo.
 const App = () => {
   // Cria estados para autenticação, visibilidade da barra de navegação, componente atual, e índice do carrossel.
@@ -69,37 +49,6 @@ const App = () => {
     navigate("/qrcode-generator")
   }
 
-  // // Função para definir o componente atual a ser exibido e atualizar o índice do carrossel.
-  // const handleAccess = (index, component) => {
-  //   setCarouselIndex(index)
-  //   setCurrentComponent(component)
-  // }
-
-  // // Função para retornar ao carrossel principal.
-  // const handleReturn = () => {
-  //   setCurrentComponent(null)
-  // }
-
-  // Função para renderizar o componente atual com base no estado.
-  // const renderComponent = () => {
-  //   switch (currentComponent) {
-  //     case "QRCodeGenerator":
-  //       return <QRCodeGenerator />
-  //     case "IPAddressFinder":
-  //       return <IPAddressFinder />
-  //     case "MovieSearchEngine":
-  //       return <MovieSearchEngine />
-  //     case "TodoApp":
-  //       return <TodoApp />
-  //     case "QuizApp":
-  //       return <QuizApp />
-  //     case "LanguageTranslator":
-  //       return <LanguageTranslator />
-  //     default:
-  //       return null
-  //   }
-  // }
-
   // Renderiza o componente principal.
   return (
     <AppContainer>
@@ -108,25 +57,7 @@ const App = () => {
           <Login onLogin={handleLogin} />
         </MainContent>
       ) : (
-        <>
-          {/* <Navbar />
-          <MainContent> */}
-          {/* 
-            {currentComponent ? (
-              <>
-                {renderComponent()}
-                <ReturnButton onClick={handleReturn}>
-                  <FaArrowLeft /> Return
-                </ReturnButton>
-              </>
-            ) : (
-             
-            )}
-            */}
-          <Outlet />
-          {/* <Footer />
-          </MainContent> */}
-        </>
+        <Outlet />
       )}
     </AppContainer>
   )
