c5b09cd93baa7c6968103497135db02a4d7ef930
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-11-06 00:30:27 -0300

    refactor: Add NoArgs and AllArgs constructor

--- modulo08/Atividade16/src/main/java/com/example/Atividade16/dto/ProductDTO.java
+++ modulo08/Atividade16/src/main/java/com/example/Atividade16/dto/ProductDTO.java
@@ -1,8 +1,12 @@
 package com.example.Atividade16.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
   private String name;
   private Double price;
